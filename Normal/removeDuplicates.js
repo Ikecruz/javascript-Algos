4713b2d585f7e344384a5856b97b03ed7c9eae85
Ikecruz <ikedinobicruz7@gmail.com>
2022-02-18 13:16:40 +0100

    remove duplicate modified 2-18-2022

--- Normal/removeDuplicates.js
+++ Normal/removeDuplicates.js
@@ -7,37 +7,15 @@
 */
 
 // Test array
-const arr = [2, 3, 6, 7, 2, 3 ,6 , 6, 10]
+const testArr = [2, 3, 6, 7, 2, 3 ,6 , 6, 10]
 
 // Solution 1
-const removeDup = (array) => {
-    // Create a new array that will contain the elements that only appeared once
-    newarr = [];
+const removeDup = (arr) => {
+    
+    return arr.map(char => char.toString()).filter(char => arr.filter(newchar => newchar == char).length == 1 ).map(Number)
 
-    // Loop through array
-    for (let i = 0; i < array.length; i++) {
-        // Set count for each array element (Since it's a loop count is 0 for each element)
-        let count = 0;
-
-        // Loop through array again (Like creating a duplicate of the array so we can compare the element we're looping with all elements in the array)
-        for (let j = 0; j < array.length; j++) {
-            
-            // Increment the element's count whenever element is found in the duplicate array
-            if (array[i] == array[j]) {
-                count++;
-            }
-
-        }
-
-        // Add the element to the new array if it occured only once
-        if (count === 1) {
-            newarr.push(array[i])
-        }
-    }
-
-    return newarr;
 }
 
-console.log(removeDup(arr))
+console.log(removeDup(testArr))
 
 // dev @Ikecruz
