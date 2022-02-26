1f4490f1f421dede4aa4256be0b26fc5538e30f8
Ikecruz <ikedinobicruz7@gmail.com>
2022-02-26 19:59:45 +0100

    Recursive Sum Mul 2-26-2022

--- Normal/recursiveSumMul.js
+++ Normal/recursiveSumMul.js
@@ -1,3 +1,33 @@
+/*
+
+	Given an array of 2k integers (for some integer k, which will be betwen 0 and 5 inclusive), perform the following operations until the array contains only one element:
+
+	On the 1st, 3rd, 5th, etc. iterations (1-based) replace each pair of consecutive elements with their sum;
+	On the 2nd, 4th, 6th, etc. iterations replace each pair of consecutive elements with their product.
+	After the algorithm has finished, there will be a single element left in the array. Return that element.
+
+	Specification
+	Challenge.packArray(arr)
+	Parameters
+	arr: Integer[] - An array of integers to be packed
+
+	Return Value
+	Integer - The final product
+
+	Constraints
+	arr.length == 2k
+
+	0 ≤ k ≤ 5
+
+	-9 ≤ arr[i] ≤ 99
+
+	Example
+	For arr = [1, 2, 3, 4, 5, 6, 7, 8], the output should be 186.
+
+	We have [1, 2, 3, 4, 5, 6, 7, 8] -> [3, 7, 11, 15] -> [21, 165] -> [186], so the answer is 186.
+
+*/
+
 const sumMul = (arr) => {
 
     if (arr.length == 0) return []
